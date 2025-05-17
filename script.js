001fb90e24c1a205c9cc538d401629ffbdc1ad20
Jahswill <jahswillakpemi@gmail.com>
2025-05-17 21:38:41 +0100

    new

---
+++ script.js
@@ -0,0 +1,37 @@
+/* Variable : used to store Data. 
+   types of variables : var (old and only used for old browser) , let , const.
+*/
+
+/* let x = 5.5;
+const y = 10;
+let name = 'Destiny' ;
+name = 'wilmus'
+console.log('His name is',name)
+
+let z = x * y
+
+
+console.log(z)
+*/
+
+let name = "Destiny"
+let age = 26
+const isStudent = false;
+
+
+//Object 
+
+const Destiny = {
+    surName: "Edo",
+    otherName: 'Benin',
+    age: 32,
+};
+
+console.log( 5 == '5') //Equals to
+console.log( 5 === '5')  // strict equals to
+console.log( 5 != '5')  // Not equals to
+console.log( 6 > 5 )   // Greater than
+console.log( 2 < 5 )   // Lesser than
+console.log( 5 <= 5 )   // Lesser than or equal to
+console.log( 5 >= 5 )   // Greater than or equal to
+
